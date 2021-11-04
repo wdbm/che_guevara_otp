7b7fc78bdbc8722aa9f78b344d8ef790f96be68b
Will Breaden Madden <wbm@protonmail.ch>
2021-11-04 02:57:19 +0000

    update

--- che_guevara_otp/__init__.py
+++ che_guevara_otp/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""
+'''
 ################################################################################
 #                                                                              #
 # che_guevara_otp                                                              #
@@ -31,7 +31,7 @@
 # <http://www.gnu.org/licenses/>.                                              #
 #                                                                              #
 ################################################################################
-"""
+'''
 
 import base64
 import datetime
@@ -45,22 +45,22 @@ import time
 
 import che_guevara_otp
 
-name    = "che_guevara_otp"
-version = "2018-05-11T1653Z"
+name        = 'che_guevara_otp'
+__version__ = '2021-11-04T0210Z'
 
 def HOTP(
     secret        = None, # secret value known to both server and client
     nonce         = 0,    # number incremented between each token generation
     leading_zeros = True  # return a string with leading zeros
     ):
-    """
+    '''
     Generate and return a hash-based one-time passcode.
-    """
+    '''
     key     = base64.b32decode(secret, True)
-    message = struct.pack(">Q", nonce)
+    message = struct.pack('>Q', nonce)
     HMAC    = hmac.new(key, message, hashlib.sha1).digest()
     o       = (HMAC[19] if sys.version_info >= (3,) else ord(HMAC[19])) & 15
-    HMAC    = (struct.unpack(">I", HMAC[o:o + 4])[0] & 0x7fffffff) % 1000000
+    HMAC    = (struct.unpack('>I', HMAC[o:o + 4])[0] & 0x7fffffff) % 1000000
     if leading_zeros:
         HMAC = str(HMAC).zfill(6)
     return HMAC
@@ -70,10 +70,10 @@ def TOTP(
     interval      = 30,   # number of seconds between each token generation
     leading_zeros = True  # return a string with leading zeros
     ):
-    """
+    '''
     Generate and return a time-based one-time passcode, changed at an interval
     of a specified number of seconds.
-    """
+    '''
     return HOTP(
         secret        = secret,
         nonce         = int(int(time.time()) / interval),
@@ -81,63 +81,55 @@ def TOTP(
     )
 
 def loop_display_TOTP_passcodes(
-    filepath_secrets  = "~/.secrets",
+    filepath_secrets  = '~/.secrets',
     set_terminal_size = True
     ):
-    """
+    '''
     Access secrets stored in specified file and loop display time-based one-time
     passcodes based on those secrets every 30 seconds.
-    """
+    '''
     filepath_secrets = os.path.expanduser(filepath_secrets)
     if not os.path.isfile(filepath_secrets):
-        print("no secrets file {filepath_secrets} found".format(
-            filepath_secrets = filepath_secrets)
-        )
+        print('no secrets file {filepath_secrets} found'.format(filepath_secrets=filepath_secrets))
         sys.exit()
     while True:
         secrets = {}
         for line in open(filepath_secrets):
-            if line not in ["", "\n"]:
-                if line[0] != "#":
-                    data = line.rstrip("\n").split(":")
+            if line not in ['', '\n']:
+                if line[0] != '#':
+                    data = line.rstrip('\n').split(':')
                     secrets[data[0].strip()] = data[1].strip()
         keys_sorted = natural_sort_list(list(secrets.keys()))
         _secrets = [(key, secrets[key]) for key in keys_sorted]
-        os.system("clear")
+        os.system('clear')
         for entry in _secrets:
-            print(entry[0] + "\n" + che_guevara_otp.TOTP(secret = entry[1]) + "\n")
+            print(entry[0] + '\n' + che_guevara_otp.TOTP(secret=entry[1]) + '\n')
         seconds = datetime.datetime.now().second
         while seconds not in [0, 31]:
             if set_terminal_size:
                 resize_terminal(
-                    width  = len(max(list(secrets.keys()), key = len)),
+                    width  = len(max(list(secrets.keys()), key=len)),
                     height = len(secrets) * 3 + 1
                 )
             seconds = datetime.datetime.now().second
-            sys.stdout.write("\r")
+            sys.stdout.write('\r')
             if seconds >= 0 and seconds <= 30:
                 seconds_remaining = 30 - seconds
             else:
                 seconds_remaining = 60 - seconds
-            sys.stdout.write("{seconds_remaining} s".format(
+            sys.stdout.write('{seconds_remaining} s'.format(
                 seconds_remaining = str(seconds_remaining).zfill(2)
             ))
             sys.stdout.flush()
             time.sleep(0.5)
 
-def natural_sort_list(
-    list_object
-    ):
+def natural_sort_list(list_object):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
-    alphanumeric_key = lambda key: [
-        convert(text) for text in re.split("([0-9]+)", key)
-    ]
-    return sorted(list_object, key = alphanumeric_key)
+    alphanumeric_key = lambda key: [convert(text) for text in re.split('([0-9]+)', key)]
+    return sorted(list_object, key=alphanumeric_key)
 
 def resize_terminal(
     width  = None,
     height = None
     ):
-    sys.stdout.write(
-        "\x1b[8;{height};{width}t".format(height = height, width = width)
-    )
+    sys.stdout.write('\x1b[8;{height};{width}t'.format(height=height, width=width))
--- setup.py
+++ setup.py
@@ -6,36 +6,34 @@ import setuptools
 
 def main():
     setuptools.setup(
-        name                 = "che_guevara_otp",
-        version              = "2018.05.11.1653",
-        description          = "Python OTP",
+        name                 = 'che_guevara_otp',
+        version              = '2021.11.04.0210',
+        description          = 'Python OTP',
         long_description     = long_description(),
-        url                  = "https://github.com/wdbm/che_guevara_otp",
-        author               = "Will Breaden Madden",
-        author_email         = "wbm@protonmail.ch",
-        license              = "GPLv3",
+        url                  = 'https://github.com/wdbm/che_guevara_otp',
+        author               = 'Will Breaden Madden',
+        author_email         = 'wbm@protonmail.ch',
+        license              = 'GPLv3',
         packages             = setuptools.find_packages(),
         entry_points         = {
-                               "console_scripts": (
-                                  "che_guevara_otp=che_guevara_otp.__init__:loop_display_TOTP_passcodes"
+                               'console_scripts': (
+                                  'che_guevara_otp=che_guevara_otp.__init__:loop_display_TOTP_passcodes'
                                )
                                },
         include_package_data = True,
         zip_safe             = False
     )
 
-def long_description(
-    filename = "README.md"
-    ):
+def long_description(filename='README.md'):
     if os.path.isfile(os.path.expandvars(filename)):
         try:
             import pypandoc
-            long_description = pypandoc.convert_file(filename, "rst")
+            long_description = pypandoc.convert_file(filename, 'rst')
         except ImportError:
             long_description = open(filename).read()
     else:
-        long_description = ""
+        long_description = ''
     return long_description
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
